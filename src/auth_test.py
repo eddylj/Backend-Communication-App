32163f6ff541b8367dfcf77c7abc84ee1ae37aaa
Mark <mark.huang@student.unsw.edu.au>
2020-09-26 00:13:31 +1000

    login logout test added

--- src/auth_test.py
+++ src/auth_test.py
@@ -1,4 +1,4 @@
-import auth
+from auth import *
 
 def test_auth_logout_success():
 #	assert auth.auth_logout(None) == False
@@ -7,3 +7,10 @@ def test_auth_logout_success():
 
 def test_auth_logout_fail():
 	assert auth.auth_logout("online") == {'is_success': True,}
+
+
+def test_auth_reg_login_logout():
+    {u_id, token} = auth_register("mechy5@gmail.com", "lolol", "Eddy", "Zhang")
+    assert auth_login("mechy5@gmail.com", "lolol") == {u_id, token}
+    assert auth_logout(token) == {True} # is_success == True
+
