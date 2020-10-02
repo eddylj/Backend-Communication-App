ea0c5f65a54f7b58e8cc28e95fdafa2f6c82bf4d
Mark <mark.huang@student.unsw.edu.au>
2020-10-02 20:15:48 +1000

    started implementation on channels.py

--- src/channels.py
+++ src/channels.py
@@ -1,6 +1,17 @@
 from data import data
+from error import InputError
 
 def channels_list(token):
+    
+    token_user = ''
+
+    for user in data['users']:
+        if user['email'] == token:
+            token_user = 
+
+
+
+
     return {
         'channels': [
         	{
@@ -11,16 +22,35 @@ def channels_list(token):
     }
 
 def channels_listall(token):
-    return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
-    }
+
+    return data['channels']
 
 def channels_create(token, name, is_public):
+    
+    # If name of the channel is longer than 20 characters
+    if len(name) > 20:
+        raise InputError
+
+    # Finding the creator/owner of the channel
+    owner = {}
+
+    for user in data['users']:
+        # For now, the token is the equivalent to the user's email
+        if user['email'] == token:
+            owner = user
+
+
+    new_channel = {
+        'id' : len(data['channels']) + 1,
+        'name' : name,
+        # 'is_public' : is_public,
+        # 'owner' : owner,
+        # 'members' : [owner],
+    }
+    
+    # Add to the global variable
+    data['channels'].append(new_channel)
+
     return {
-        'channel_id': 1,
+        'id': len(data['channels']),
     }
