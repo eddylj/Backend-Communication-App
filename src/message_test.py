b0968605de96f56123862854e1852b12bc02916d
Mark Huang <z5308487@ad.unsw.edu.au>
2020-10-14 15:19:59 +1100

    message_send working on

--- src/message_test.py
+++ src/message_test.py
@@ -3,3 +3,26 @@ import pytest
 from error import InputError
 from other import clear
 from data import data
+
+
+############################### MESSAGE_SEND TESTS ###############################
+
+def test_message_send_too_long():
+    message = "what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do what it do what it do what it do \
+                what it do what it do what it do "
+    
