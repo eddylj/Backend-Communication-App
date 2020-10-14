5552b3f175643bdc12d1dc33fd07050aae17c6ac
Mark Huang <z5308487@ad.unsw.edu.au>
2020-10-14 15:28:43 +1100

    tests

--- src/message_test.py
+++ src/message_test.py
@@ -3,11 +3,12 @@ import pytest
 from error import InputError
 from other import clear
 from data import data
+from other import create_account
 
 
 ############################### MESSAGE_SEND TESTS ###############################
 
-def test_message_send_too_long():
+def test_message_send_too_long(create_account):
     message = "what it do what it do what it do what it do what it do what it do \
                 what it do what it do what it do what it do what it do what it do \
                 what it do what it do what it do what it do what it do what it do \
