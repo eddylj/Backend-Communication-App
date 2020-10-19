e61b77267c98809de3d6b8483be88b924ed7088d
Mark Huang <z5308487@ad.unsw.edu.au>
2020-10-19 21:40:06 +1100

    pylint compliant made

--- src/message_test.py
+++ src/message_test.py
@@ -1,22 +1,29 @@
-import message
-import auth, channel, channels
+'''
+Tests for all functions in message.py
+'''
 import pytest
-from error import InputError
+import message
+import auth
+import channel
+import channels
+from error import InputError, AccessError
 from other import clear
 from data import data
-from other import create_account
+#from other import create_account
 
 
 ############################### MESSAGE_SEND TESTS ###############################
 
 def test_message_send_error_tests():
+    '''
+    Tests to test the errors from different case using message_send
+    '''
     clear()
 
     # Create 2 users
     user1 = ('validemail@gmail.com', '123abc!@#', 'Hayden', 'Everest')
     account1 = auth.auth_register(*user1)
     token1 = account1['token']
-    u_id1 = account1['u_id']
 
     user2 = ('alsovalid@gmail.com', 'aW5Me@l!', 'Andras', 'Arato')
     account2 = auth.auth_register(*user2)
@@ -42,7 +49,7 @@ def test_message_send_error_tests():
             what it do what it do what it do what it do what it do what it do what it do what it do \
             what it do what it do what it do what it do what it do what it do what it do what it do \
             what it do what it do what it do what it do "
-    
+
     # Message is too long
     with pytest.raises(InputError):
         message.message_send(token1, channel_id, message_error)
@@ -58,7 +65,4 @@ def test_message_send_error_tests():
 
     message_id2 = message.message_send(token2, channel_id, message_valid)
     assert message_id2['message_id'] == 2
-
-
-
     
