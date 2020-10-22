588cd95df864cdc53b21d98997f7317728f3ff70
Mark Huang <z5308487@ad.unsw.edu.au>
2020-10-22 17:03:58 +1100

    doing implem

--- src/message.py
+++ src/message.py
@@ -8,6 +8,48 @@ def message_remove(token, message_id):
     }
 
 def message_edit(token, message_id, message):
-    
+    # If the message is too long
+    if len(message) > 1000:
+        raise InputError
+
+    u_id = get_active(token)
+    if u_id == None:
+        raise AccessError
+
+
     return {
     }
+
+# def message_send(token, channel_id, message):
+#     # Could be error if we change what token is, right now it is the stringed version of u_id
+#     # If the message is too long
+#     if len(message) > 1000:
+#         raise InputError
+
+#     u_id = get_active(token)
+#     if u_id == None:
+#         raise AccessError
+
+#     # If the user is not part of the channel, and not owner of flockr
+#     if u_id not in data['channels'][channel_id]['members'] and u_id != 0:
+#         raise AccessError
+
+#     message_id = len(data['messages'])
+#     new_message = {
+#         'message_id' : message_id,
+#         'u_id' : u_id,
+#         'message' : message,
+#         'time_created' : 0,
+#         'channel_id' : channel_id,
+#     }
+
+#     # add to main database
+#     data['messages'].append(new_message)
+
+#     # also add to channels database
+#     # This puts the new message at the 0th element, as per requirement in channel_messages
+#     data['channels'][channel_id]['messages'].insert(0, new_message)
+    
+#     return {
+#         'message_id': message_id,
+#     }
