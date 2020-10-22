df0f7e82e530da9f001e42ffadaf94feaa360e5e
Mark Huang <z5308487@ad.unsw.edu.au>
2020-10-22 22:14:40 +1100

    pylint

--- src/echo_test.py
+++ src/echo_test.py
@@ -1,12 +1,21 @@
-import echo
+'''
+echo test
+'''
 import pytest
+import echo
 from error import InputError
 
 def test_echo():
+    '''
+    test echo
+    '''
     assert echo.echo("1") == "1", "1 == 1"
     assert echo.echo("abc") == "abc", "abc == abc"
     assert echo.echo("trump") == "trump", "trump == trump"
 
 def test_echo_except():
+    '''
+    test echo except
+    '''
     with pytest.raises(InputError):
         assert echo.echo("echo")
