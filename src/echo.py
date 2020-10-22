5f42327ebd5b6afcc29aedbb89828f9de155e938
Mark Huang <z5308487@ad.unsw.edu.au>
2020-10-22 22:04:26 +1100

    pylint

--- src/echo.py
+++ src/echo.py
@@ -1,6 +1,12 @@
+'''
+Echo
+'''
 from error import InputError
 
 def echo(value):
+    '''
+    Test echo
+    '''
     if value == 'echo':
         raise InputError('Input cannot be echo')
     return value
