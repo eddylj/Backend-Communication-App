c64dd6d4f502834a5a304cf42c22fcc03e999790
Mark Huang <z5308487@ad.unsw.edu.au>
2020-10-22 17:12:10 +1100

    done message_edit implementation

--- src/message.py
+++ src/message.py
@@ -16,40 +16,19 @@ def message_edit(token, message_id, message):
     if u_id == None:
         raise AccessError
 
+    ogu_id = data['messages'][message_id]['u_id']
+
+    # If not original sender, not owner and not owner of flockr
+    if u_id != ogu_id and u_id not in data['channels'][channel_id]['owners'] and u_id != 0:
+        raise AccessError
+
+    # Delete if message is an empty string
+    if message == "":
+        data['messages'][message_id] = {}
+    else:
+        # edit message
+        data['messages'][message_id]['message'] = message
+
 
     return {
     }
-
-# def message_send(token, channel_id, message):
-#     # Could be error if we change what token is, right now it is the stringed version of u_id
-#     # If the message is too long
-#     if len(message) > 1000:
-#         raise InputError
-
-#     u_id = get_active(token)
-#     if u_id == None:
-#         raise AccessError
-
-#     # If the user is not part of the channel, and not owner of flockr
-#     if u_id not in data['channels'][channel_id]['members'] and u_id != 0:
-#         raise AccessError
-
-#     message_id = len(data['messages'])
-#     new_message = {
-#         'message_id' : message_id,
-#         'u_id' : u_id,
-#         'message' : message,
-#         'time_created' : 0,
-#         'channel_id' : channel_id,
-#     }
-
-#     # add to main database
-#     data['messages'].append(new_message)
-
-#     # also add to channels database
-#     # This puts the new message at the 0th element, as per requirement in channel_messages
-#     data['channels'][channel_id]['messages'].insert(0, new_message)
-    
-#     return {
-#         'message_id': message_id,
-#     }
