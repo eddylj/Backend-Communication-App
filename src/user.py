bda5d22321d9efff6b2925961b36985359e23094
Mark Huang <z5308487@ad.unsw.edu.au>
2020-10-22 19:01:03 +1100

    user implementation done

--- src/user.py
+++ src/user.py
@@ -1,22 +1,111 @@
+from data import data
+from error import InputError, AccessError
+from other import get_active
+
 def user_profile(token, u_id):
+    '''
+    Return information on the user (u_id, email, first name, last name, handle)
+    '''
+
+    if u_id >= len(data['users']):
+        raise InputError
+
+    user = data['users'][u_id]
+
+    return_user = {
+        'u_id' : user['u_id'],
+        'email' : user['email'],
+        'name_first' : user['name_first'],
+        'name_last' : user['name_last'],
+        'handle_str' : user['handle'],
+    }
+
     return {
-        'user': {
-        	'u_id': 1,
-        	'email': 'cs1531@cse.unsw.edu.au',
-        	'name_first': 'Hayden',
-        	'name_last': 'Jacobs',
-        	'handle_str': 'hjacobs',
-        },
+        'user': return_user,
     }
 
 def user_profile_setname(token, name_first, name_last):
+    '''
+    Change first or last name of the user if valid 
+    '''
+    # name_first invalid length
+    if len(name_first) > 50 or len(name_first) < 1:
+        raise InputError
+
+    # name_last invalid length
+    if len(name_last) > 50 or len(name_last) < 1:
+        raise InputError
+
+    # Don't use get_active function as user doesn't have to be active
+    u_id = int(token)
+
+    data['users'][u_id]['name_first'] = name_first
+    data['users'][u_id]['name_last'] = name_last
+    
+
     return {
     }
 
 def user_profile_setemail(token, email):
+    '''
+    Change email of the user
+    '''
+    # Check if valid email
+    if not is_valid(email):
+        raise InputError
+
+    # Not used by another account
+    for user in data['users']:
+        if user['email'] == email:
+            raise InputError
+    
+    # Don't use get_active function as user doesn't have to be active
+    u_id = int(token)
+
+    data['users'][u_id]['email'] = email
+
     return {
     }
 
 def user_profile_sethandle(token, handle_str):
+    '''
+    Change handle of the user
+    '''
+
+    # Invalid length
+    if len(handle_str) < 3 or len(handle_str) > 20:
+        raise InputError
+    
+    # Not used by another account
+    for user in data['users']:
+        if user['handle'] == handle_str:
+            raise InputError
+
+    # Don't use get_active function as user doesn't have to be active
+    u_id = int(token)
+
+    data['users'][u_id]['handle'] = handle_str
+
     return {
     }
+
+
+def is_valid(email):
+    """
+    Code provided in project specs, from:
+    https://www.geeksforgeeks.org/check-if-email-address-valid-or-not-in-python/
+    Checks if email is valid against a regular expression.
+
+    Parameters:
+        email (str) : User's email
+
+    Returns:
+        (bool): Whether or not the email entered is invalid according to the
+                regex standards.
+    """
+    regex = '^[a-z0-9]+[\\._]?[a-z0-9]+[@]\\w+[.]\\w{2,3}$'
+    if(re.search(regex,email)):
+        return True
+          
+    else:  
+        return False
