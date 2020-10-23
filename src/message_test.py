7b908a6424c0aee84a7ca4acdb47747d5853bdb9
William <lin.william91@gmail.com>
2020-10-23 18:34:11 +1100

    Message_edit() black-box tests, pylint compliant

--- src/message_test.py
+++ src/message_test.py
@@ -46,26 +46,26 @@ def test_message_send_valid():
     timestamp2 = int(time.time())
     msg_id2 = message.message_send(token2, channel_id, "Goodbye")['message_id']
 
-    expected = {
-        'messages': [
-            {
-                'message_id': msg_id1,
-                'u_id': u_id1,
-                'message': "Hello",
-                'time_created': timestamp1
-            },
-            {
-                'message_id': msg_id2,
-                'u_id': u_id2,
-                'message': "Goodbye",
-                'time_created': timestamp2
-            }
-        ],
+    expected = [
+        {
+            'message_id': msg_id1,
+            'u_id': u_id1,
+            'message': "Hello",
+            'time_created': timestamp1
+        },
+        {
+            'message_id': msg_id2,
+            'u_id': u_id2,
+            'message': "Goodbye",
+            'time_created': timestamp2
+        }
+    ]
+
+    assert channel.channel_messages(token1, channel_id, 0) == {
+        'messages': expected,
         'start': 0,
         'end': -1
     }
-
-    assert channel.channel_messages(token1, channel_id, 0) == expected
 
     # Will move this to channel_messages
     # User2 leaves the channel
@@ -128,6 +128,8 @@ def test_message_send_not_member():
 
 ############################# MESSAGE_REMOVE TESTS #############################
 
+# Can't call channel_messages if start < number of messages in channel.
+# Keep this in mind if tests break.
 def test_message_remove_valid():
     """ Base case for message_remove() """
     clear()
@@ -199,20 +201,20 @@ def test_message_remove_not_owner():
     with pytest.raises(AccessError):
         message.message_remove(token2, msg_id)
 
-    expected = {
-        'messages': [
-            {
-                'message_id': msg_id,
-                'u_id': u_id1,
-                'message': "Hello",
-                'time_created': timestamp
-            }
-        ],
+    expected = [
+        {
+            'message_id': msg_id,
+            'u_id': u_id1,
+            'message': "Hello",
+            'time_created': timestamp
+        }
+    ]
+
+    assert channel.channel_messages(token1, channel_id, 0) == {
+        'messages': expected,
         'start': 0,
         'end': -1
     }
-
-    assert channel.channel_messages(token1, channel_id, 0) == expected
 
 def test_message_remove_as_owner():
     """
@@ -261,11 +263,183 @@ def test_message_remove_not_member():
     # Create channel
     channel_id = channels.channels_create(token2, "Testing", True)['channel_id']
 
-    msg_id = message.message_send(token2, channel_id, "Hello")['message_id']
+    msg_id = message.message_send(token2, channel_id, "Goodbye")['message_id']
 
     with pytest.raises(AccessError):
         message.message_remove(token1, msg_id)
 
-############################# CHANNELS_LIST TESTS ##############################
+############################## MESSAGE_EDIT TESTS ##############################
+
+# Assumed that edit updates the timestamp.
+
+def test_message_edit_valid():
+    """
+    Base case for message_edit(). Editing a message normally and checking
+    against channel_messages().
+    """
+    clear()
+
+    account = auth.auth_register(*user)
+    token = account['token']
+    u_id = account['u_id']
+
+    channel_id = channels.channels_create(token, "Testing", True)['channel_id']
+
+    timestamp = int(time.time())
+    msg_id = message.message_send(token, channel_id, "Hello")['message_id']
+
+    expected = [
+        {
+            'message_id': msg_id,
+            'u_id': u_id,
+            'message': "Hello",
+            'time_created': timestamp
+        }
+    ]
+
+    assert channel.channel_messages(token, channel_id, 0) == {
+        'messages': expected,
+        'start': 0,
+        'end': -1
+    }
+
+    timestamp = int(time.time())
+    message.message_edit(token, msg_id, "Goodbye")
+
+    expected[0]['message'] = "Goodbye"
+    expected[0]['time_created'] = timestamp
+    assert channel.channel_messages(token, channel_id, 0) == {
+        'messages': expected,
+        'start': 0,
+        'end': -1
+    }
+
+def test_message_edit_empty():
+    """
+    Test case for message_edit(), where the passed string is empty. Should
+    delete the message as per specification.
+    """
+    clear()
+
+    account = auth.auth_register(*user)
+    token = account['token']
+
+    channel_id = channels.channels_create(token, "Testing", True)['channel_id']
+
+    msg_id = message.message_send(token, channel_id, "Hello")['message_id']
+
+    message.message_edit(token, msg_id, "")
+
+    assert channel.channel_messages(token, channel_id, 0) == {
+        'messages': [],
+        'start': 0,
+        'end': -1
+    }
+
+def test_message_edit_not_owner():
+    """
+    Test case for message_edit(), where the caller isn't the user who sent the
+    message, or an owner of the channel/Flockr.
+    """
+    clear()
+
+    # Create 2 users
+    account1 = auth.auth_register(*user1)
+    token1 = account1['token']
+    u_id1 = account1['u_id']
+
+    account2 = auth.auth_register(*user2)
+    token2 = account2['token']
+    u_id2 = account2['u_id']
+
+    # Create channel
+    channel_id = channels.channels_create(token1, "Testing", True)['channel_id']
+
+    # Invite user 2 into the channel
+    channel.channel_invite(token1, channel_id, u_id2)
+
+    timestamp = int(time.time())
+    msg_id = message.message_send(token1, channel_id, "Hello")['message_id']
+
+    expected = [
+        {
+            'message_id': msg_id,
+            'u_id': u_id1,
+            'message': "Hello",
+            'time_created': timestamp
+        }
+    ]
+
+    with pytest.raises(AccessError):
+        message.message_edit(token2, msg_id, "Goodbye")
+
+    assert channel.channel_messages(token1, channel_id, 0) == {
+        'messages': expected,
+        'start': 0,
+        'end': -1
+    }
+
+def test_message_edit_as_owner():
+    """
+    Testing if an owner of the flockr or channel can freely edit messages.
+    """
+    clear()
+
+    # Create 2 users
+    account1 = auth.auth_register(*user1)
+    token1 = account1['token']
+
+    account2 = auth.auth_register(*user2)
+    token2 = account2['token']
+    u_id2 = account2['u_id']
+
+    # Create channel
+    channel_id = channels.channels_create(token1, "Testing", True)['channel_id']
+
+    # Invite user 2 into the channel
+    channel.channel_invite(token1, channel_id, u_id2)
+
+    # User 2 sends a message, then user 1 edits it.
+    msg_id = message.message_send(token2, channel_id, "Goodbye")['message_id']
+    timestamp = int(time.time())
+    message.message_edit(token1, msg_id, "Hello")
+
+    expected = [
+        {
+            'message_id': msg_id,
+            # Should we replace the u_id with the one of the latest editor?
+            'u_id': u_id2,
+            'message': "Hello",
+            'time_created': timestamp
+        }
+    ]
+
+    assert channel.channel_messages(token1, channel_id, 0) == {
+        'messages': expected,
+        'start': 0,
+        'end': -1
+    }
+
+def test_message_edit_not_member():
+    """
+    Edge case for message_edit(), where the caller isn't even in the channel
+    where the message is sent. This includes the Flockr owner.
+    """
+    clear()
+
+    # Create 2 users
+    account1 = auth.auth_register(*user1)
+    token1 = account1['token']
+
+    account2 = auth.auth_register(*user2)
+    token2 = account2['token']
+
+    # Create channel
+    channel_id = channels.channels_create(token2, "Testing", True)['channel_id']
+
+    msg_id = message.message_send(token2, channel_id, "Goodbye")['message_id']
+
+    with pytest.raises(AccessError):
+        message.message_edit(token1, msg_id, "Hello")
 
 clear()
