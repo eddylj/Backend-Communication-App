076060d8b2ad64ba4d3b0320e6cdda1efa6eb624
Mark Huang <z5308487@ad.unsw.edu.au>
2020-10-25 18:57:04 +1100

    permission id added and implemented and changed some tests

--- src/message.py
+++ src/message.py
@@ -64,9 +64,9 @@ def message_remove(token, message_id):
     channel_data = data['channels'][channel_id]
 
     # If not sender of message / not owner of flockr
-    if u_id not in (data['messages'][message_id]['u_id'], 0):
+    if u_id != data['messages'][message_id]['u_id']:
         if u_id not in channel_data['owners']:
-            if data['users'][caller_id]['permission_id'] != 1:
+            if data['users'][u_id]['permission_id'] != 1:
                 raise AccessError
     elif u_id not in channel_data['members']:
         raise AccessError
@@ -109,8 +109,8 @@ def message_edit(token, message_id, message):
     channel_messages = data['channels'][channel_id]['messages']
 
     # If not original sender, not owner and not owner of flockr
-    if u_id not in (sender_id, data['messages'][message_id]['u_id'], 0):
-        if data['users'][caller_id]['permission_id'] != 1:
+    if u_id not in (sender_id, data['messages'][message_id]['u_id']):
+        if data['users'][u_id]['permission_id'] != 1:
             raise AccessError
 
     for (index, msg) in enumerate(channel_messages):
