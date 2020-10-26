0b87b66c6d332504c726fd9444d2dc5b4efc18aa
William <lin.william91@gmail.com>
2020-10-26 14:22:45 +1100

    Replaced url imports with fixtures in each http_test file

--- src/message_server_test.py
+++ src/message_server_test.py
@@ -1,8 +1,35 @@
 """
 This module contains tests for message routes in server.py.
 """
+import re
+from subprocess import Popen, PIPE
+from time import sleep
+import signal
+import pytest
 import requests
-from echo_http_test import url
+
+@pytest.fixture
+def url():
+    '''
+    Fixture for creating a server
+    '''
+    url_re = re.compile(r' \* Running on ([^ ]*)')
+    server = Popen(["python3", "server.py"], stderr=PIPE, stdout=PIPE)
+    line = server.stderr.readline()
+    local_url = url_re.match(line.decode())
+    if local_url:
+        yield local_url.group(1)
+        # Terminate the server
+        server.send_signal(signal.SIGINT)
+        waited = 0
+        while server.poll() is None and waited < 5:
+            sleep(0.1)
+            waited += 0.1
+        if server.poll() is None:
+            server.kill()
+    else:
+        server.kill()
+        raise Exception("Couldn't get URL from local server")
 
 user = {
     'email': 'validemail@gmail.com',
